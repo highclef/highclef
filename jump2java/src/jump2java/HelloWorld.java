3f7ccfb86be299c380b3f2b77b50ef1d7ff21170
high_ <high_@DESKTOP-CLFTE7B.fritz.box>
2018-11-05 16:22:56 +0100

    현재 시각 출력

--- jump2java/src/jump2java/HelloWorld.java
+++ jump2java/src/jump2java/HelloWorld.java
@@ -3,5 +3,6 @@ package jump2java;
 public class HelloWorld {
 	public static void main(String args[]) {
         System.out.println("Hello World");
+        System.out.println("Time is 1:06 AM");
     }
 }
