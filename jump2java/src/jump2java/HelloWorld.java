ed22b6c074c9ebb870010e7609fe225a7a79fc17
highclef <high_clef@naver.com>
2018-11-05 16:26:13 +0100

    브랜치 test에 출력 코드 추가

--- jump2java/src/jump2java/HelloWorld.java
+++ jump2java/src/jump2java/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 	public static void main(String args[]) {
         System.out.println("Hello World");
         System.out.println("Time is 1:06 AM");
+        System.out.println("Soooooo hot!!");
     }
 }
