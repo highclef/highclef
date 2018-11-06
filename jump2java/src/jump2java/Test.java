1f0e64e50784d07b2277506fb42359d4ffaa3caa
highclef <high_clef@naver.com>
2018-11-06 15:14:44 +0100

    06.11.2018

---
+++ jump2java/src/jump2java/Test.java
@@ -0,0 +1,23 @@
+package jump2java;
+
+public class Test {
+	public static void main(String[] args) {
+		// append method
+        StringBuffer sb = new StringBuffer();
+        sb.append("1. hello");
+        sb.append(" ");
+        sb.append("jump to java");
+        System.out.println(sb.toString());
+        
+        // insert method
+        StringBuffer sb2 = new StringBuffer();
+        sb2.append("jump to java");
+        sb2.insert(0, "2. hello ");
+        System.out.println(sb2.toString());
+        
+        // substring method
+        StringBuffer sb3 = new StringBuffer();
+        sb3.append("Hello jump to java");
+        System.out.println(sb3.substring(0, 4));
+    }
+}
