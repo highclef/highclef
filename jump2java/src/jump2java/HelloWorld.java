8b9f33c93e87c7e1524ff159ea10d1426012015a
highclef <high_clef@naver.com>
2018-11-06 14:02:54 +0100

    Error Fixed @ HelloWorld.java

--- jump2java/src/jump2java/HelloWorld.java
+++ jump2java/src/jump2java/HelloWorld.java
@@ -4,10 +4,8 @@ public class HelloWorld {
 	public static void main(String args[]) {
         System.out.println("Hello World");
         System.out.println("Time is 1:06 AM");
-<<<<<<< HEAD
+
         System.out.println("아름다운 밤이야이야");
-=======
         System.out.println("Soooooo hot!!");
->>>>>>> refs/heads/test
-    }
+        }
 }
