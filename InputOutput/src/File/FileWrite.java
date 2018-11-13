a703556efe1a10d9e459dea1cb1d118903252f4f
highclef <high_clef@naver.com>
2018-11-13 14:51:33 +0100

    13.11.2018

--- InputOutput/src/File/FileWrite.java
+++ InputOutput/src/File/FileWrite.java
@@ -1,9 +1,14 @@
 package File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.FileWriter;
+import java.io.PrintWriter;
 
 public class FileWrite {
     public static void main(String[] args) throws IOException {
+    	/*
+    	 * 
+    	// FileOutputStream - 바이트 단위로 데이터를 처리함
         FileOutputStream output = new FileOutputStream("c:/out.txt");
         for(int i=1; i<11; i++) {
             String data = i+" 번째 줄입니다.\r\n";
@@ -12,5 +17,22 @@ public class FileWrite {
             // FileOutputStream에 값을 쓸 때는 byte배열로 써야 함
         }
         output.close();
+        
+    	// FileWriter - 문자열을 직접 파일에 쓸 수 있다
+        FileWriter fw = new FileWriter("c:/out.txt");
+        for(int i=1; i<11; i++) {
+            String data = i+" 번째 줄입니다.\r\n";
+            fw.write(data);
+        }
+        fw.close();
+        */
+    	
+    	// PrintWriter - 자동 줄바꿈
+        PrintWriter pw = new PrintWriter("c:/out.txt");
+        for(int i=1; i<11; i++) {
+            String data = i+" 번째 줄입니다.";
+            pw.println(data);
+        }
+        pw.close();
     }
 }
